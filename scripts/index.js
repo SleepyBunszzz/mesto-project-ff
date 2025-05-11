e8c1d1d8284a4e336a2df89b05af0bce502394d1
Anastasiya Kryazhevskikh <AnastasiyaKr0609@yandex.ru>
2025-05-11 13:11:58 +0300

    проект 5_JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
+const cardTemplateContent = document                             
+  .querySelector('#card-template')
+  .content;
 // @todo: Функция создания карточки
-
+function createCard({ name, link }, onDelete) {
+    const cardElement = cardTemplateContent
+      .querySelector('.card')
+      .cloneNode(true);
+      const cardImage = cardElement.querySelector('.card__image');
+      const cardTitle = cardElement.querySelector('.card__title');
+      const deleteBtn = cardElement.querySelector('.card__delete-button');
+      cardImage.src = link;
+      cardImage.alt = name;
+      cardTitle.textContent = name;
+      deleteBtn.addEventListener('click', () => onDelete(cardElement));
+    return cardElement;
+}
 // @todo: Функция удаления карточки
+function removeCard(cardElement) {
+    cardElement.remove();
+  }
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, removeCard);
+    placesList.append(cardElement);
+  });
